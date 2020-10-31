7620946bbba1d52eb2c30cbe7ec87fff96d0e674
AssaadZiade <71119895+AssaadZiade@users.noreply.github.com>
2020-10-31 20:44:08 +0200

    Update signIn.php

--- signIn.php
+++ signIn.php
@@ -1,1 +1,25 @@
 
+<?php
+  $connection = new mysqli("localhost", "root", "", "AjaxLab");
+
+   if ($connection->connect_error) {
+  die("Connection failed: " . $connection->connect_error);
+}
+
+   $username = $_POST["username"];
+   $password = $_POST["password"];
+
+       $sql = "SELECT * FROM users WHERE name = '$username' AND password = '$password'";
+
+       $result = mysqli_query($connection, $sql);
+
+       if (mysqli_num_rows($result) == 0){
+           echo "\nWrong Username Or Password! Please Try Again";
+       } else {
+           echo "\n\nLogged In";
+       }
+    
+
+
+
+?>
