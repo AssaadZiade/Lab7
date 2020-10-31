4a8f0d4fcfed0c506708fb4fd642f08c6169b6bd
AssaadZiade <71119895+AssaadZiade@users.noreply.github.com>
2020-10-31 20:42:49 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,15 +1,32 @@
 <?php
-include("database.php");
+   $connection = new mysqli("localhost", "root", "", "AjaxLab");
 
-if($_SERVER["REQUEST_METHOD"] == "GET"){
-    $username = $_POST["usernamePHP"];
-    $password = $_POST["passwordPHP" ];
-    $ccpassword = $_POST["ccpasswordPHP"];
-    $birthday=  $_POST["birthdayPHP"];
-
-    connectDb($servername,$username,$password,$dbname);
-    
-
+   if ($connection->connect_error) {
+  die("Connection failed: " . $connection->connect_error);
 }
+
+mysqli_query($connection, "DELETE FROM users");
+
+   $username = $_POST["username"];
+   $password = $_POST["password"];
+   $ccpassword = $_POST["ccpassword"];
+   $birthday = $_POST["birthday"];
+
+   if ($password != $ccpassword){
+       echo "\nAttention! Passwords don't match! ";
+   } else {
+
+       $sql = "SELECT * FROM users WHERE name = '$username' AND password = '$password'";
+
+       $result = mysqli_query($connection, $sql);
+
+       if (mysqli_num_rows($result) != 0){
+           echo "\nAttention!Can't Sign In Because User Already Exists";
+       } else {
+           mysqli_query($connection, "INSERT INTO users VALUES ('$username', '$password')");
+           echo "\n\nSUCCESS";
+       }
+    }
+
 
 ?>
